268c40a4c921562713540abd407818d293bec4e9
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-06-16 17:34:10 +0530

    added basic wallet connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import { connect, disconnect } from "@argent/get-starknet";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [account, setAccount] = useState(null);
+  async function connectWallet() {
+    const connection = await connect();
+    if (connection && connection.account) {
+      setAccount(connection.account);
+    }
+  }
+  return <button onClick={connectWallet}>connect</button>;
 }
 
 export default App;
