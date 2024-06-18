9a46683c33e07984c3c807310ee4651aa591ade7
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-06-18 13:29:23 +0530

    added connection to wallet for L1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
 import "./App.css";
-import { connect, disconnect } from "@argent/get-starknet";
+import { connect } from "@argent/get-starknet";
 function App() {
   const [account, setAccount] = useState(null);
-  async function connectWallet() {
+  async function connectWalletL2() {
     const connection = await connect();
     if (connection && connection.account) {
       setAccount(connection.account);
-      localStorage.setItem("account",account);
+      localStorage.setItem("account", account);
     }
   }
-  return <button onClick={connectWallet}>connect</button>;
+  async function connectWalletL1() {
+    try {
+      const { ethereum } = window;
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+    } catch (error) {
+      alert(error);
+    }
+  }
+  return <button onClick={connectWalletL2}>connect</button>;
 }
 
 export default App;
