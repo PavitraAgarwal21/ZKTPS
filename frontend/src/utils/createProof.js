fb3e2e49ba908ca443146fd8fdc482f6548ebffc
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-06-18 14:14:51 +0530

    minor fixes

--- frontend/src/utils/createProof.js
+++ frontend/src/utils/createProof.js
@@ -1,23 +1,21 @@
 const snarkjs = require("snarkjs");
 export const generateProof = async (
   nullifier,
-  nullifierHash,
-  address,
   secret,
+  nullifierHash,
   commitmentHash
 ) => {
   const input = {
     nullifier: nullifier,
-    nullifierHash: nullifierHash,
-    recipient: address,
     secret: secret,
+    nullifierHash: nullifierHash,
     commitment: commitmentHash,
   };
 
   const { proof, publicSignals } = await snarkjs.groth16.fullProve(
     input,
-    "Withdraw.wasm",
-    "Withdraw_0001.zkey"
+    "Ticket.wasm",
+    "Ticket_0001.zkey"
   );
   return proof;
 };
