ccf0be1eecea5f4a6cdb87c1ac920cdae628fb78
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-06-20 12:01:48 +0530

    adding joining of event and creation of url

--- frontend/src/components/createEvent.js
+++ frontend/src/components/createEvent.js
@@ -1,8 +1,14 @@
 import { shortString } from "starknet";
-import { getL2contract, getL2provider, toDecimal } from "../web3/web3";
+import {
+  getL2contract,
+  getL2contractRead,
+  getL2provider,
+  toDecimal,
+} from "../web3/web3";
 import random from "../utils/random";
-
+import { useNavigate } from "react-router-dom";
 export default function CreateEvent(props) {
+  const history = useNavigate();
   async function create_event() {
     const token_address = document.querySelector("#address").value;
     const provider = getL2provider();
@@ -23,14 +29,27 @@ export default function CreateEvent(props) {
         token_address
       );
       console.log(tx);
-      const transactionHash = tx.transaction_hash;
-      console.log(transactionHash);
-      const txReceipt = await provider.waitForTransaction(transactionHash);
-      console.log(txReceipt);
-      const listEvents = txReceipt.events;
-      console.log(listEvents);
+      history(`home/${event_index}`);
+      // const transactionHash = tx.transaction_hash;
+      // console.log(transactionHash);
+      // const txReceipt = await provider.waitForTransaction(transactionHash);
+      // console.log(txReceipt);
+      // const listEvents = txReceipt.events;
+      // console.log(listEvents);
     } catch (error) {
       alert(error);
+    }
+  }
+  async function join_event() {
+    const event_index = document.querySelector("#eventId").value;
+    const contract = getL2contractRead();
+    const tx = await contract.getEventdetails(event_index);
+    console.log(tx);
+    if (tx.creator == "0") {
+      alert("Invalid event_index");
+      return;
+    } else {
+      history(`home/${event_index}`);
     }
   }
   return (
@@ -40,6 +59,8 @@ export default function CreateEvent(props) {
       <input type="text" placeholder="enter number of tickets" id="tickets" />
       <input type="text" placeholder="enter token address here" id="address" />
       <button onClick={create_event}>create</button>
+      <input type="text" placeholder="enter the event_index" id="eventId" />
+      <button onClick={join_event}>join</button>
     </div>
   );
 }
