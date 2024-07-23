89110deb1d837deb3d3e714254a8c41774e30741
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-07-23 10:26:56 +0530

    removed redundancy of scanner

---
+++ frontend/src/components/Scanner.js
@@ -0,0 +1,156 @@
+import { QrReader } from "react-qr-reader";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import { isMobile } from "react-device-detect";
+import { Tooltip } from "@mui/material";
+import Button from "@mui/material/Button";
+import { useContext, useState } from "react";
+import { getData } from "../web3/web3";
+import { storeContext } from "../useContext/storeContext";
+
+/**
+ * @typedef {Object} QRReaderProps
+ * @property {(d: string) => void} setData - Function to set the scanned data
+ * @property {(msg: string) => void} handleError - Function to handle errors
+ * @property {() => void} handleClose - Function to handle the close event
+ */
+
+// Usage example
+/**
+ * @param {QRReaderProps} props
+ */
+
+const QRReader = (props) => {
+  const facingMode = isMobile ? "environment" : "user";
+  const { account } = useContext(storeContext);
+  return (
+    <div style={{ width: "100%" }}>
+      <QrReader
+        ViewFinder={ViewFinder}
+        constraints={{ facingMode }}
+        onResult={async (result, error) => {
+          await getData(result, error, props, account);
+        }}
+      />
+    </div>
+  );
+};
+
+/**
+ * @typedef {Object} ScanNoteDialogProps
+ * @property {boolean} open - Indicates if the dialog is open
+ * @property {() => void} onClose - Function to handle the close event
+ * @property {(d: string) => void} setData - Function to set the scanned data
+ * @property {(msg: string) => void} handleError - Function to handle errors
+ * @property {string} dialogTitle - The title of the dialog
+ */
+
+// Usage example
+/**
+ * @param {ScanNoteDialogProps} props
+ */
+function ScanNoteDialog(props) {
+  const { onClose, open } = props;
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>{props.dialogTitle}</DialogTitle>
+      <QRReader
+        handleClose={handleClose}
+        handleError={props.handleError}
+        func={props.func}
+      ></QRReader>
+    </Dialog>
+  );
+}
+
+/**
+ * @typedef {Object} ScanNoteButtonProps
+ * @property {(d: string) => void} setData - Function to set the scanned data
+ * @property {(msg: string) => void} handleError - Function to handle errors
+ * @property {string} dialogTitle - The title of the dialog
+ */
+
+// Usage example
+/**
+ * @param {ScanNoteButtonProps} props
+ */
+export function ScanNoteButton(props) {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Tooltip arrow title="Scan a QR code">
+        <Button
+          variant="contained"
+          sx={{ height: "100%", fontWeight: 800 }}
+          onClick={handleClickOpen}
+        >
+          Scan
+        </Button>
+      </Tooltip>
+      <ScanNoteDialog
+        open={open}
+        onClose={handleClose}
+        handleError={props.handleError}
+        dialogTitle={props.dialogTitle}
+        func={props.func}
+      />
+    </div>
+  );
+}
+export const ViewFinder = () => (
+  <>
+    <svg
+      width="50px"
+      viewBox="0 0 100 100"
+      style={{
+        top: 0,
+        left: 0,
+        zIndex: 1,
+        boxSizing: "border-box",
+        border: "50px solid rgba(0, 0, 0, 0.3)",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <path
+        fill="none"
+        d="M13,0 L0,0 L0,13"
+        stroke="rgba(255, 0, 0, 0.5)"
+        strokeWidth="5"
+      />
+      <path
+        fill="none"
+        d="M0,87 L0,100 L13,100"
+        stroke="rgba(255, 0, 0, 0.5)"
+        strokeWidth="5"
+      />
+      <path
+        fill="none"
+        d="M87,100 L100,100 L100,87"
+        stroke="rgba(255, 0, 0, 0.5)"
+        strokeWidth="5"
+      />
+      <path
+        fill="none"
+        d="M100,13 L100,0 87,0"
+        stroke="rgba(255, 0, 0, 0.5)"
+        strokeWidth="5"
+      />
+    </svg>
+  </>
+);
--- frontend/src/components/invalidateTicket.js
+++ frontend/src/components/invalidateTicket.js
@@ -1,10 +1,4 @@
 import { generateProof } from "../utils/createProof";
-import { QrReader } from "react-qr-reader";
-import DialogTitle from "@mui/material/DialogTitle";
-import Dialog from "@mui/material/Dialog";
-import { isMobile } from "react-device-detect";
-import { Tooltip } from "@mui/material";
-import Button from "@mui/material/Button";
 import {
   Invalidate,
   connectWalletL1,
@@ -14,173 +8,8 @@ import {
   toHex,
   L1_Contract_Address,
 } from "../web3/web3";
-import { useState } from "react";
-import { on } from "process";
 import { toast } from "react-toastify";
-
-/**
- * @typedef {Object} QRReaderProps
- * @property {(d: string) => void} setData - Function to set the scanned data
- * @property {(msg: string) => void} handleError - Function to handle errors
- * @property {() => void} handleClose - Function to handle the close event
- */
-
-// Usage example
-/**
- * @param {QRReaderProps} props
- */
-
-const QRReader = (props) => {
-  const facingMode = isMobile ? "environment" : "user";
-  return (
-    <div style={{ width: "100%" }}>
-      <QrReader
-        ViewFinder={ViewFinder}
-        constraints={{ facingMode }}
-        onResult={async (result, error) => {
-          await getData(result, error, props);
-        }}
-      />
-    </div>
-  );
-};
-
-/**
- * @typedef {Object} ScanNoteDialogProps
- * @property {boolean} open - Indicates if the dialog is open
- * @property {() => void} onClose - Function to handle the close event
- * @property {(d: string) => void} setData - Function to set the scanned data
- * @property {(msg: string) => void} handleError - Function to handle errors
- * @property {string} dialogTitle - The title of the dialog
- */
-
-// Usage example
-/**
- * @param {ScanNoteDialogProps} props
- */
-function ScanNoteDialog(props) {
-  const { onClose, open } = props;
-
-  const handleClose = () => {
-    onClose();
-  };
-
-  return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>{props.dialogTitle}</DialogTitle>
-      <QRReader
-        handleClose={handleClose}
-        setData={props.setData}
-        handleError={props.handleError}
-      ></QRReader>
-    </Dialog>
-  );
-}
-
-/**
- * @typedef {Object} ScanNoteButtonProps
- * @property {(d: string) => void} setData - Function to set the scanned data
- * @property {(msg: string) => void} handleError - Function to handle errors
- * @property {string} dialogTitle - The title of the dialog
- */
-
-// Usage example
-/**
- * @param {ScanNoteButtonProps} props
- */
-export function ScanNoteButton1(props) {
-  const [open, setOpen] = useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <div>
-      <Tooltip arrow title="Scan a QR code">
-        <Button
-          variant="contained"
-          sx={{ height: "100%", fontWeight: 800 }}
-          onClick={handleClickOpen}
-        >
-          Scan
-        </Button>
-      </Tooltip>
-      <ScanNoteDialog
-        open={open}
-        onClose={handleClose}
-        setData={props.setData}
-        handleError={props.handleError}
-        dialogTitle={props.dialogTitle}
-      />
-    </div>
-  );
-}
-export const ViewFinder = () => (
-  <>
-    <svg
-      width="50px"
-      viewBox="0 0 100 100"
-      style={{
-        top: 0,
-        left: 0,
-        zIndex: 1,
-        boxSizing: "border-box",
-        border: "50px solid rgba(0, 0, 0, 0.3)",
-        position: "absolute",
-        width: "100%",
-        height: "100%",
-      }}
-    >
-      <path
-        fill="none"
-        d="M13,0 L0,0 L0,13"
-        stroke="rgba(255, 0, 0, 0.5)"
-        strokeWidth="5"
-      />
-      <path
-        fill="none"
-        d="M0,87 L0,100 L13,100"
-        stroke="rgba(255, 0, 0, 0.5)"
-        strokeWidth="5"
-      />
-      <path
-        fill="none"
-        d="M87,100 L100,100 L100,87"
-        stroke="rgba(255, 0, 0, 0.5)"
-        strokeWidth="5"
-      />
-      <path
-        fill="none"
-        d="M100,13 L100,0 87,0"
-        stroke="rgba(255, 0, 0, 0.5)"
-        strokeWidth="5"
-      />
-    </svg>
-  </>
-);
-async function getData(result, error, props) {
-  if (!!result) {
-    alert("Qr Scanned Successful");
-    props.handleClose();
-    try {
-      const values = result?.text.split(",");
-      const nullifier = parseInt(values[0]);
-      const secret = parseInt(values[1]);
-      const nullifierHash = values[2];
-      const commitmentHash = values[3];
-      // how we get the values of the recipient from the one who is calling the function ,
-      // so we what to know the address of recipient which is the one who is calling the function
-      await invalidateTicket(nullifier, secret, nullifierHash, commitmentHash);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
+import { ScanNoteButton } from "./Scanner";
 
 const fetchDataContract = async (commitment) => {
   const url =
@@ -229,7 +58,7 @@ function handleInvalidatedTicketEvent(event, buyer) {
   // Add your custom code here
 }
 
-async function invalidateTicket(
+export async function invalidateTicket(
   nullifier,
   secret,
   nullifierHash,
@@ -294,7 +123,10 @@ export default function InvalidateTicket() {
       <div className="flex flex-col items-center mt-4">
         <h3 className="text-white">Invalidate Ticket</h3>
         <div className="flex justify-center mt-4">
-          <ScanNoteButton1 dialogTitle="Scan for invalidate ticket" />
+          <ScanNoteButton
+            dialogTitle="Scan for invalidate ticket"
+            func={"invalidate"}
+          />
         </div>
       </div>
     </div>
